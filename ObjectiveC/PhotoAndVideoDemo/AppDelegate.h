9445488a52988e28b47733bb2707577aff25e0d5
cjch <350765427@qq.com>
2015-11-26 14:02:24 +0800

    init

---
+++ ObjectiveC/PhotoAndVideoDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PhotoAndVideoDemo
+//
+//  Created by jiechen on 15/11/26.
+//  Copyright © 2015年 jiechen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ObjectiveC/PhotoAndVideoDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  PhotoAndVideoDemo
+//
+//  Created by jiechen on 15/11/26.
+//  Copyright © 2015年 jiechen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
